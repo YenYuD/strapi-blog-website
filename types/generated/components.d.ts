0f7a852263d0e7d1d6f96ef71f589a9cc8b8db3a
YenYu <diaoemily0830@gmail.com>
2024-08-14 15:18:58 +0800

    feat: meta component in article table

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SeoComponentsMeta extends Schema.Component {
+  collectionName: 'components_seo_components_metas';
+  info: {
+    displayName: 'meta';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'seo-components.meta': SeoComponentsMeta;
+    }
+  }
 }
