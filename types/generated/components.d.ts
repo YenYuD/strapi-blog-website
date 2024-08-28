800be044c1b8eb3b1597cbaf2fefdf2c817cf586
YenYu <diaoemily0830@gmail.com>
2024-08-28 17:09:06 +0800

    feat: full documentation, type documentation

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,40 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SidebarSub extends Schema.Component {
+  collectionName: 'components_sidebar_subs';
+  info: {
+    displayName: 'second-sub';
+    icon: 'arrowRight';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    articles: Attribute.Relation<
+      'sidebar.sub',
+      'oneToMany',
+      'api::article.article'
+    >;
+  };
+}
+
+export interface SidebarSubItem extends Schema.Component {
+  collectionName: 'components_sidebar_sub_items';
+  info: {
+    displayName: 'sub-item';
+    icon: 'arrowRight';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    articles: Attribute.Relation<
+      'sidebar.sub-item',
+      'oneToMany',
+      'api::article.article'
+    >;
+    sub: Attribute.Component<'sidebar.sub', true>;
+  };
+}
+
 export interface SeoComponentsMeta extends Schema.Component {
   collectionName: 'components_seo_components_metas';
   info: {
@@ -15,6 +50,8 @@ export interface SeoComponentsMeta extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'sidebar.sub': SidebarSub;
+      'sidebar.sub-item': SidebarSubItem;
       'seo-components.meta': SeoComponentsMeta;
     }
   }
